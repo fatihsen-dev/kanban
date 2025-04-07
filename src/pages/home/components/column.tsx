22fa236acb5be865a3a634ca578de8ef27145f56
fatihsen-dev <fatihydrm@outlook.com>
2025-04-07 17:55:31 +0300

    refactor: simplify layout of Column component and adjust DropdownMenu positioning

--- src/pages/home/components/column.tsx
+++ src/pages/home/components/column.tsx
@@ -37,26 +37,26 @@ export default function Column({ column, taskId, setTaskId }: ColumnProps) {
          className={`bg-gray-50 border-2 border-dashed border-gray-300 rounded-md p-4 min-w-96 h-full flex flex-col gap-4 transition-colors ${
             isOver ? "bg-gray-200" : ""
          }`}>
-         <div className='flex justify-between tasks-center border-b border-gray-300 pb-4'>
-            <div className='flex flex-col gap-2'>
+         <div className='flex flex-col w-full'>
+            <div className='w-full flex tasks-center justify-between gap-2'>
                <h2 className='text-lg font-bold'>{column.title}</h2>
-               <span className='text-sm text-gray-500'>{column.tasks.length} tasks</span>
+               <DropdownMenu>
+                  <DropdownMenuTrigger className='cursor-pointer'>
+                     <Ellipsis className='w-full! h-full!' />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent side='left' align='start' sideOffset={0} alignOffset={0}>
+                     <DropdownMenuLabel>Column</DropdownMenuLabel>
+                     <DropdownMenuSeparator />
+                     <DropdownMenuItem
+                        className='cursor-pointer'
+                        variant='destructive'
+                        onClick={() => store.column.remove(column.id)}>
+                        Delete
+                     </DropdownMenuItem>
+                  </DropdownMenuContent>
+               </DropdownMenu>
             </div>
-            <DropdownMenu>
-               <DropdownMenuTrigger className='cursor-pointer'>
-                  <Ellipsis className='w-full! h-full!' />
-               </DropdownMenuTrigger>
-               <DropdownMenuContent side='left' align='start' sideOffset={0} alignOffset={0}>
-                  <DropdownMenuLabel>Column</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem
-                     className='cursor-pointer'
-                     variant='destructive'
-                     onClick={() => store.column.remove(column.id)}>
-                     Delete
-                  </DropdownMenuItem>
-               </DropdownMenuContent>
-            </DropdownMenu>
+            <span className='text-sm text-gray-500'>{column.tasks.length} tasks</span>
          </div>
          <div className='flex flex-col gap-2 flex-1'>
             {column.tasks.map((task) => (
